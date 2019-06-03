452ea04f8a6fb70640b12b32cc029780ea6d887d
Kenneth Wong <kenneth_wong@dlsu.edu.ph>
2019-06-03 22:22:55 +0800

    asdasdsad

--- gentelella-master/globemaster/SupplierOrderDetails.php
+++ gentelella-master/globemaster/SupplierOrderDetails.php
@@ -65,8 +65,14 @@
                     <div class = "col-md-6" align = "right">
                         Expected Date of Arrival: 04-01-12 to 04-15-12
                     </div>  
-                    <div class = "col-md-12" align = "center">
-                    *Progress bar here*git 
+                    <div class = "col-md-12; container1" align = "center">
+                        <ul class="progressbar">
+                            <li class="active">Purchased</li>
+                            <li>Shipping</li>
+                            <li>Delivered</li>
+                        </ul>
+                    <br>
+                    <font color = "#4192f4">View Details v</font>
                     </div>
                     <div>                    
                     <table id="datatable-responsive" class="table table-striped table-bordered dt-responsive nowrap" cellspacing="0" width="100%">
@@ -159,5 +165,59 @@
 
     </style>    
 
+    <style>
+            .container1 {
+            width: 600px;
+            margin: 100px auto; 
+        }
+        .progressbar {
+            counter-reset: step;
+        }
+        .progressbar li {
+            list-style-type: none;
+            width: 33.3%;
+            float: left;
+            font-size: 12px;
+            position: relative;
+            text-align: center;
+            text-transform: uppercase;
+            color: #7d7d7d;
+        }
+        .progressbar li:before {
+            width: 30px;
+            height: 30px;
+            content: counter(step);
+            counter-increment: step;
+            line-height: 30px;
+            border: 2px solid #7d7d7d;
+            display: block;
+            text-align: center;
+            margin: 0 auto 10px auto;
+            border-radius: 50%;
+            background-color: white;
+        }
+        .progressbar li:after {
+            width: 100%;
+            height: 2px;
+            content: '';
+            position: absolute;
+            background-color: #7d7d7d;
+            top: 15px;
+            left: -50%;
+            z-index: -1;
+        }
+        .progressbar li:first-child:after {
+            content: none;
+        }
+        .progressbar li.active {
+            color: green;
+        }
+        .progressbar li.active:before {
+            border-color: #55b776;
+        }
+        .progressbar li.active + li:after {
+            background-color: #55b776;
+        }
+    </style>
   </body>
 </html>
